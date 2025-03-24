d4c77d43b6793104f2f81acea1ae96cc130bf46e
Avishka Priyasad <avishkapriyasad11@gmail.com>
2025-03-24 22:16:42 +0530

    initial commit

---
+++ travel/src/Pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      Hey i am about page
+    </div>
+  )
+}
+
+export default About
---
+++ travel/src/Pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      Hey I am contact page
+    </div>
+  )
+}
+
+export default Contact
